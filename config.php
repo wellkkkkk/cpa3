9f44cfd6d2169d7407e99b2530680410055317b7
wellkkkkk <welllol732@gmail.com>
2023-04-17 14:38:48 -0400

    first commit

---
+++ config.php
@@ -0,0 +1,21 @@
+<?php
+
+    $dbHost = 'Localhost';
+    $dbUsername = 'root';
+    $dbPassword = '';
+    $dbName = 'cpa';
+
+    $conexao = new mysqli ($dbHost,$dbUsername,$dbPassword,$dbName);
+
+
+//     if($conexao->connect_errno)
+//     {
+//         echo "Erro"; 
+//     }
+
+//     else
+//     {
+//         echo "Conexão Efetuada com Sucesso.";
+//     }
+
+// ?>
